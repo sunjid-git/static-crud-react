20df99da5cdd6491e893dad00585e2b230626314
sunjid-git <sunjid.info@gmail.com>
2021-09-28 10:46:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div className="container">
+            <div className="py-4">
+                <h1>About Page</h1>
+                <div className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed quaerat quod eos ut, eligendi laboriosam qui inventore omnis nihil, ad molestias aliquam quam dolorum consequuntur tenetur cupiditate suscipit. Exercitationem.
+                </div>
+                <div className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed quaerat quod eos ut, eligendi laboriosam qui inventore omnis nihil, ad molestias aliquam quam dolorum consequuntur tenetur cupiditate suscipit. Exercitationem.
+                </div>
+                <div className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sed quaerat quod eos ut, eligendi laboriosam qui inventore omnis nihil, ad molestias aliquam quam dolorum consequuntur tenetur cupiditate suscipit. Exercitationem.
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
